7f0de55f24202ae8ebbdf36e92049192924c4166
MJ Deng <mjdeng@fb.com>
2022-11-15 08:23:13 -0800

    Extend NativeExecutionOperator to launch the native process

--- presto-spark-base/src/main/java/com/facebook/presto/spark/execution/PrestoSparkTaskExecutorFactory.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/execution/PrestoSparkTaskExecutorFactory.java
@@ -202,6 +202,7 @@ public class PrestoSparkTaskExecutorFactory
     private final AtomicBoolean memoryRevokePending = new AtomicBoolean();
     private final AtomicBoolean memoryRevokeRequestInProgress = new AtomicBoolean();
     private final BlockEncodingSerde blockEncodingSerde;
+    private final NativeExecutionProcessFactory processFactory;
 
     @Inject
     public PrestoSparkTaskExecutorFactory(
@@ -227,7 +228,8 @@ public class PrestoSparkTaskExecutorFactory
             TempStorageManager tempStorageManager,
             PrestoSparkBroadcastTableCacheManager prestoSparkBroadcastTableCacheManager,
             PrestoSparkConfig prestoSparkConfig,
-            BlockEncodingSerde blockEncodingSerde)
+            BlockEncodingSerde blockEncodingSerde,
+            NativeExecutionProcessFactory processFactory)
     {
         this(
                 sessionPropertyManager,
@@ -256,7 +258,8 @@ public class PrestoSparkTaskExecutorFactory
                 tempStorageManager,
                 requireNonNull(prestoSparkConfig, "prestoSparkConfig is null").getStorageBasedBroadcastJoinStorage(),
                 prestoSparkBroadcastTableCacheManager,
-                blockEncodingSerde);
+                blockEncodingSerde,
+                processFactory);
     }
 
     public PrestoSparkTaskExecutorFactory(
@@ -286,7 +289,8 @@ public class PrestoSparkTaskExecutorFactory
             TempStorageManager tempStorageManager,
             String storageBasedBroadcastJoinStorage,
             PrestoSparkBroadcastTableCacheManager prestoSparkBroadcastTableCacheManager,
-            BlockEncodingSerde blockEncodingSerde)
+            BlockEncodingSerde blockEncodingSerde,
+            NativeExecutionProcessFactory processFactory)
     {
         this.sessionPropertyManager = requireNonNull(sessionPropertyManager, "sessionPropertyManager is null");
         this.blockEncodingManager = requireNonNull(blockEncodingManager, "blockEncodingManager is null");
@@ -316,6 +320,7 @@ public class PrestoSparkTaskExecutorFactory
         this.storageBasedBroadcastJoinStorage = requireNonNull(storageBasedBroadcastJoinStorage, "storageBasedBroadcastJoinStorage is null");
         this.prestoSparkBroadcastTableCacheManager = requireNonNull(prestoSparkBroadcastTableCacheManager, "prestoSparkBroadcastTableCacheManager is null");
         this.blockEncodingSerde = requireNonNull(blockEncodingSerde, "blockEncodingSerde is null");
+        this.processFactory = requireNonNull(processFactory, "processFactory is null");
     }
 
     @Override
@@ -592,7 +597,7 @@ public class PrestoSparkTaskExecutorFactory
                         stageId),
                 taskDescriptor.getTableWriteInfo(),
                 true,
-                ImmutableList.of(new NativeExecutionOperator.NativeExecutionOperatorTranslator(session, fragment, blockEncodingSerde)));
+                ImmutableList.of(new NativeExecutionOperator.NativeExecutionOperatorTranslator(session, fragment, blockEncodingSerde, processFactory)));
 
         taskStateMachine.addStateChangeListener(state -> {
             if (state.isDone()) {
--- presto-spark-base/src/main/java/com/facebook/presto/spark/execution/operator/NativeExecutionOperator.java
+++ presto-spark-base/src/main/java/com/facebook/presto/spark/execution/operator/NativeExecutionOperator.java
@@ -13,10 +13,12 @@
  */
 package com.facebook.presto.spark.execution.operator;
 
+import com.facebook.airlift.log.Logger;
 import com.facebook.presto.Session;
 import com.facebook.presto.common.Page;
 import com.facebook.presto.common.block.BlockEncodingSerde;
 import com.facebook.presto.execution.ScheduledSplit;
+import com.facebook.presto.execution.TaskInfo;
 import com.facebook.presto.execution.TaskSource;
 import com.facebook.presto.execution.buffer.PagesSerdeFactory;
 import com.facebook.presto.execution.scheduler.TableWriteInfo;
@@ -27,9 +29,12 @@ import com.facebook.presto.operator.OperatorFactory;
 import com.facebook.presto.operator.SourceOperator;
 import com.facebook.presto.operator.SourceOperatorFactory;
 import com.facebook.presto.operator.SplitOperatorInfo;
+import com.facebook.presto.spark.execution.NativeExecutionProcess;
+import com.facebook.presto.spark.execution.NativeExecutionProcessFactory;
 import com.facebook.presto.spark.execution.NativeExecutionTask;
 import com.facebook.presto.spi.UpdatablePageSource;
 import com.facebook.presto.spi.page.PagesSerde;
+import com.facebook.presto.spi.page.SerializedPage;
 import com.facebook.presto.spi.plan.PlanNode;
 import com.facebook.presto.spi.plan.PlanNodeId;
 import com.facebook.presto.sql.planner.LocalExecutionPlanner;
@@ -37,10 +42,15 @@ import com.facebook.presto.sql.planner.PlanFragment;
 import com.facebook.presto.sql.planner.plan.InternalPlanVisitor;
 import com.facebook.presto.sql.planner.plan.NativeExecutionNode;
 import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 
 import static com.facebook.presto.SystemSessionProperties.isExchangeChecksumEnabled;
@@ -66,16 +76,19 @@ import static java.util.Objects.requireNonNull;
 public class NativeExecutionOperator
         implements SourceOperator
 {
+    private static final Logger log = Logger.get(NativeExecutionOperator.class);
+    private static final String NATIVE_EXECUTION_SERVER_URI = "http://127.0.0.1";
     private final PlanNodeId sourceId;
     private final OperatorContext operatorContext;
     private final LocalMemoryContext systemMemoryContext;
-    // TODO: will be used to create the {@link NativeExecutionTask}
     private final PlanFragment planFragment;
-    // TODO: will be used to create the {@link NativeExecutionTask}
     private final TableWriteInfo tableWriteInfo;
-    // TODO: will be used to deserialize the  {@link SerializedPage}
     private final PagesSerde serde;
     private TaskSource taskSource;
+    private final NativeExecutionProcessFactory processFactory;
+    private NativeExecutionProcess process;
+    private NativeExecutionTask task;
+    private CompletableFuture<Void> taskStatusFuture;
     private boolean finished;
 
     public NativeExecutionOperator(
@@ -83,7 +96,8 @@ public class NativeExecutionOperator
             OperatorContext operatorContext,
             PlanFragment planFragment,
             TableWriteInfo tableWriteInfo,
-            PagesSerde serde)
+            PagesSerde serde,
+            NativeExecutionProcessFactory processFactory)
     {
         this.sourceId = requireNonNull(sourceId, "sourceId is null");
         this.operatorContext = requireNonNull(operatorContext, "operatorContext is null");
@@ -91,6 +105,7 @@ public class NativeExecutionOperator
         this.planFragment = requireNonNull(planFragment, "planFragment is null");
         this.tableWriteInfo = requireNonNull(tableWriteInfo, "tableWriteInfo is null");
         this.serde = requireNonNull(serde, "serde is null");
+        this.processFactory = requireNonNull(processFactory, "processFactory is null");
     }
 
     @Override
@@ -112,17 +127,76 @@ public class NativeExecutionOperator
     }
 
     /**
-     * TODO: The getOutput method will call NativeExecutionTask APIs to:
-     *  1. Submit the plan to the external process
-     *  2. Call pollResult method to get latest buffered result.
-     *  3. Call getTaskInfo method to get the TaskInfo and propagate it
-     *  4. Deserialize the polled {@link SerializedPage} to {@link Page} and return it back
+     * The overall workflow of the getOutput method is:
+     * 1. Submit the plan to the external process
+     * 2. Call pollResult method to get latest buffered result.
+     * 3. Call getTaskInfo method to get the TaskInfo and propagate it
+     * 4. Deserialize the polled {@link SerializedPage} to {@link Page} and return it back
      */
     @Override
     public Page getOutput()
     {
-        finished = true;
-        return null;
+        if (finished) {
+            return null;
+        }
+
+        if (process == null) {
+            createProcess();
+            checkState(process != null, "process is null");
+            this.task = process.getTask();
+            taskStatusFuture = task.start();
+        }
+
+        try {
+            if (taskStatusFuture.isDone()) {
+                taskStatusFuture.get();
+                Optional<TaskInfo> taskInfo = task.getTaskInfo();
+                taskInfo.ifPresent(info -> info.getTaskStatus().getFailures().forEach(e -> log.error(e.toException())));
+                taskStatusFuture.get();
+                Optional<SerializedPage> page = task.pollResult();
+                if (page.isPresent()) {
+                    return processResult(page.get());
+                }
+                else {
+                    finished = true;
+                    return null;
+                }
+            }
+
+            Optional<SerializedPage> page = task.pollResult();
+            return page.map(this::processResult).orElse(null);
+        }
+        catch (InterruptedException | ExecutionException e) {
+            log.error(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createProcess()
+    {
+        checkState(taskSource != null, "taskSource is null");
+        checkState(taskStatusFuture == null, "taskStatusFuture has already been set");
+        try {
+            this.process = processFactory.createNativeExecutionProcess(
+                    operatorContext.getSession(),
+                    URI.create(NATIVE_EXECUTION_SERVER_URI),
+                    operatorContext.getDriverContext().getTaskId(),
+                    planFragment,
+                    ImmutableList.of(taskSource),
+                    tableWriteInfo);
+            process.start();
+        }
+        catch (ExecutionException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Page processResult(SerializedPage page)
+    {
+        operatorContext.recordRawInput(page.getSizeInBytes(), page.getPositionCount());
+        Page deserializedPage = serde.deserialize(page);
+        operatorContext.recordProcessedInput(deserializedPage.getSizeInBytes(), page.getPositionCount());
+        return deserializedPage;
     }
 
     @Override
@@ -181,6 +255,8 @@ public class NativeExecutionOperator
     public void close()
     {
         systemMemoryContext.setBytes(0);
+        task.stop();
+        process.close();
     }
 
     public static class NativeExecutionOperatorFactory
@@ -191,6 +267,7 @@ public class NativeExecutionOperator
         private final PlanFragment planFragment;
         private final TableWriteInfo tableWriteInfo;
         private final PagesSerdeFactory serdeFactory;
+        private final NativeExecutionProcessFactory processFactory;
         private boolean closed;
 
         public NativeExecutionOperatorFactory(
@@ -198,13 +275,15 @@ public class NativeExecutionOperator
                 PlanNodeId planNodeId,
                 PlanFragment planFragment,
                 TableWriteInfo tableWriteInfo,
-                PagesSerdeFactory serdeFactory)
+                PagesSerdeFactory serdeFactory,
+                NativeExecutionProcessFactory processFactory)
         {
             this.operatorId = operatorId;
             this.planNodeId = requireNonNull(planNodeId, "planNodeId is null");
             this.planFragment = requireNonNull(planFragment, "planFragment is null");
             this.tableWriteInfo = requireNonNull(tableWriteInfo, "tableWriteInfo is null");
             this.serdeFactory = requireNonNull(serdeFactory, "serdeFactory is null");
+            this.processFactory = requireNonNull(processFactory, "processFactory is null");
         }
 
         @Override
@@ -223,7 +302,8 @@ public class NativeExecutionOperator
                     operatorContext,
                     planFragment,
                     tableWriteInfo,
-                    serdeFactory.createPagesSerde());
+                    serdeFactory.createPagesSerde(),
+                    processFactory);
         }
 
         @Override
@@ -244,12 +324,14 @@ public class NativeExecutionOperator
         private final PlanFragment fragment;
         private final Session session;
         private final BlockEncodingSerde blockEncodingSerde;
+        private final NativeExecutionProcessFactory processFactory;
 
-        public NativeExecutionOperatorTranslator(Session session, PlanFragment fragment, BlockEncodingSerde blockEncodingSerde)
+        public NativeExecutionOperatorTranslator(Session session, PlanFragment fragment, BlockEncodingSerde blockEncodingSerde, NativeExecutionProcessFactory processFactory)
         {
             this.fragment = requireNonNull(fragment, "fragment is null");
             this.session = requireNonNull(session, "session is null");
             this.blockEncodingSerde = requireNonNull(blockEncodingSerde, "blockEncodingSerde is null");
+            this.processFactory = requireNonNull(processFactory, "processFactory is null");
         }
 
         @Override
@@ -264,7 +346,8 @@ public class NativeExecutionOperator
                         node.getId(),
                         fragment.withSubPlan(((NativeExecutionNode) node).getSubPlan()),
                         context.getTableWriteInfo(),
-                        new PagesSerdeFactory(blockEncodingSerde, isExchangeCompressionEnabled(session), isExchangeChecksumEnabled(session)));
+                        new PagesSerdeFactory(blockEncodingSerde, isExchangeCompressionEnabled(session), isExchangeChecksumEnabled(session)),
+                        processFactory);
                 return Optional.of(
                         new LocalExecutionPlanner.PhysicalOperation(operatorFactory, makeLayout(node), context, UNGROUPED_EXECUTION));
             }
